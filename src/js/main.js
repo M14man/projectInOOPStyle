a2aedb92d8953c179ca22779f7b3cb4b683057dc
Dmyto_Turovskiy <turovskydo@gmail.com>
2022-04-14 15:23:23 +0300

    all sliders are working now

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,42 @@
-import Slider from './modules/slider';
+import MainSlider from './modules/slider/slider-main';
 import VideoPlayer from './modules/videoPlayer';
+import MiniSlider from './modules/slider/slide-mini';
 
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
     
-    const slider = new Slider('.page', '.next');
+    const slider = new MainSlider({btns: '.next', container: '.page' });
     slider.render();
+
+    const showUpSlider = new MiniSlider({
+        container: '.showup__content-slider',
+        next: '.showup__next',
+        prev: '.showup__prev',
+        activeClass: 'card-active',
+        animate: true
+    });
+    showUpSlider.init();
+
+    const modulesSlider = new MiniSlider({
+        container: '.modules__content-slider',
+        next: '.modules__info-btns .slick-next',
+        prev: '.modules__info-btns .slick-prev',
+        activeClass: 'card-active',
+        animate: true,
+        autoplay: true
+    });
+    modulesSlider.init();
+
+    const feedSlider = new MiniSlider({
+        container: '.feed__slider',
+        next: '.feed__slider .slick-next',
+        prev: '.feed__slider .slick-prev',
+        activeClass: 'feed__item-active'
+    });
+    feedSlider.init();
+
+
     const videoPlayer = new VideoPlayer('.showup .play', '.overlay');
     videoPlayer.init();
 
